cb0dab9fa64b197b643f71c330b9ccffed4bf5db
tod619 <trevorodwyer619@hotmail.com>
2021-02-28 15:27:10 +0000

    complete update function in script.js

--- script.js
+++ script.js
@@ -37,7 +37,16 @@ function update () {
 
     const actives = document.querySelectorAll(".active")
 
-    console.log(actives.length, circles.length)
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 +"%"
+
+    if(currentActive === 1) {
+        prev.disabled = true
+    } else if (currentActive === circles.length) {
+        next.disabled = true
+    } else {
+        prev.disabled = false
+        next.disabled = false
+    }
 }
 
 
